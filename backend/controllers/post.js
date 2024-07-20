ebfce23e77094827e948f65cf107a969af7b80a4
ahsanawan-0 <ahsanawan0482@gmail.com>
2024-07-21 04:52:30 +0500

    adding likeunlike,deletePost api

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -9,10 +9,10 @@ module.exports={
         try {
          const newPostData={
             caption:req.body.caption,
-            // image:{
-            //     public_id:"req.body.public_id",
-            //     url:"req.body.url",
-            // },
+            image:{
+                public_id:"req.body.public_id",
+                url:"req.body.url",
+            },
             owner:req.user._id,
             
         }  
@@ -36,5 +36,91 @@ module.exports={
     
         }
         
+    },
+    likeAndUnlike:async(req,res)=>{
+    try {
+const post= await  postModel.findById(req.params.id);
+if(!post)
+{
+    return res.status(404)
+.json({
+    success:false,
+    message:"post not found"
+})
+}
+
+
+if(post.likes.includes(req.user._id)){
+    const index=post.likes.indexOf(req.user._id)
+post.likes.splice(index,1)
+await post.save();
+return res.status(200)
+.json({
+    success:true,
+    message:"post unlike"
+})
+
+}
+else{
+    post.likes.push(req.user._id);
+   await post.save();
+}
+return res.status(200)
+.json({
+    success:true,
+    message:"post liked"
+})
+
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:error.message,
+        })
     }
+    },
+
+    deletePost:async(req,res)=>{
+        try {
+            const post= await postModel.findById(req.params.id);
+            if(!post)
+                {
+                    return res.status(404)
+                .json({
+                    success:false,
+                    message:"post not found"
+                })
+                }
+
+            if(post.owner.toString()!==req.user._id.toString()){
+                return res.status(401)
+            .json({
+                success:false,
+                message:"unAuthorize"
+            })
+            }
+            await post.deleteOne()
+            const user= await userModel.findById(req.user._id)
+            const index=user.posts.indexOf(req.params.id);
+            user.posts.splice(index,1);
+            await user.save();
+
+                res.status(201)
+                .json({
+                    success:true,
+                    message:"post deleted"
+                })
+                    
+            
+        }
+        
+            
+        catch (error) {
+            res.status(500).json({
+                success:false,
+                message:error.message,
+            })
+        }
+    }
+
+
 }
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,11 +1,12 @@
 const route = require('express').Router();
 
 const { isAuthenticated } = require("../middleweres/auth");
-const { createPost } = require("../controllers/post");
+const { createPost, likeAndUnlike, deletePost } = require("../controllers/post");
 
 
 
 route.post("/createpost",isAuthenticated,createPost)
+route.get("/post/:id",isAuthenticated,likeAndUnlike).delete("/post/:id",isAuthenticated,deletePost)
 
 
 
