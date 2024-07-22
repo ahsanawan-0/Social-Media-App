14cedbc917d32304d7e8bf011f541d303683ccf5
ahsanawan-0 <ahsanawan0482@gmail.com>
2024-07-22 10:17:44 +0500

    adding ownprofile,other profile,AllProfiles ,deleteProfile,fix after deleting profile removing also followers and following array

--- backend/controllers/profile.js
+++ backend/controllers/profile.js
@@ -1,16 +1,16 @@
 const userModel = require("../models/Users");
+const postModel = require("../models/Post");
 module.exports = {
   updatePassword: async (req, res) => {
     try {
-      const user = await userModel.findById(req.user._id).select("+password")
+      const user = await userModel.findById(req.user._id).select("+password");
       const { currentPassword, updatePassword } = req.body;
-if(!currentPassword||!updatePassword){
-  return res.status(400).json({
-    success: false,
-    message: "Enter Both Current & New Password",
-  });
-    
-}
+      if (!currentPassword || !updatePassword) {
+        return res.status(400).json({
+          success: false,
+          message: "Enter Both Current & New Password",
+        });
+      }
       const isMatch = await user.matchPassword(currentPassword);
       if (!isMatch) {
         return res.status(400).json({
@@ -43,11 +43,113 @@ if(!currentPassword||!updatePassword){
       if (email) {
         user.email = email;
       }
-      await user.save()
+      await user.save();
 
       res.status(200).json({
         success: true,
         message: "  Profile  Updated Successfully",
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+  deleteProfile: async (req, res) => {
+    try {
+      const user = await userModel.findByIdAndDelete(req.user._id);
+      //get post array of user follower and id
+      const post = user.posts;
+      const following = user.following;
+      const followers = user.followes;
+      const userId = user._id;
+
+      // logout after delete
+      res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+      });
+
+      for (let i = 0; i < following.length; i++) {
+        const getFollowing = await userModel.findById(following[i]);
+
+        const index = getFollowing.followes.indexOf(userId);
+        getFollowing.followes.splice(index, 1);
+
+        await getFollowing.save();
+      }
+      for (let i = 0; i < followers.length; i++) {
+        const getFollwers = await userModel.findById(followers[i]);
+
+        const index = getFollwers.following.indexOf(userId);
+        getFollwers.following.splice(index, 1);
+
+        await getFollwers.save();
+      }
+
+      // delete posts after deleting user
+      for (let i = 0; i < post.length; i++) {
+        const getpost = await postModel.findByIdAndDelete(post[i]);
+      }
+      res.status(200).json({
+        success: true,
+        message: "Profile deleted successfully",
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+  getOwnProfile: async (req, res) => {
+    try {
+      const user = await userModel.findById(req.user._id).populate("posts");
+      res.status(200).json({
+        success: true,
+        user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+  getOtherProfile: async (req, res) => {
+    try {
+      const user = await userModel.findById(req.params.id).populate("posts");
+      if(!user){
+        res.status(500).json({
+          success: false,
+          message:"user not found",
+        });
+      }
+      res.status(200).json({
+        success: true,
+        user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+  getAllProfile: async (req, res) => {
+    try {
+      const users = await userModel.find({})
+
+      if(!users){
+        res.status(500).json({
+          success: false,
+          message:"user not found",
+        });
+      }
+      res.status(200).json({
+        success: true,
+        users,
       });
     } catch (error) {
       res.status(500).json({
--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,9 +1,13 @@
 const route = require("express").Router();
 
 const { isAuthenticated } = require("../middleweres/auth");
-const { updatePassword, updateProfile } = require("../controllers/profile");
+const { updatePassword, updateProfile, deleteProfile, getOwnProfile, getAllProfile, getOtherProfile } = require("../controllers/profile");
 
 route.put("/updatepassword", isAuthenticated, updatePassword);
+route.delete("/deleteProfile", isAuthenticated, deleteProfile);
+route.get("/getProfile", isAuthenticated, getOwnProfile);
+route.get("/getProfile/:id", isAuthenticated, getOtherProfile);
+route.get("/getAllProfiles", isAuthenticated, getAllProfile);
 route.put("/updateprofile", isAuthenticated, updateProfile);
 
 module.exports = route;
